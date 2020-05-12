e32187ccf42c22968b5973094278c0c6c6398aad
Code4Blessings <robinwarden@code4blessings.com>
2020-05-11 21:46:25 -0400

    Added stack and queues folder along with files

---
+++ Stacks _and_Queues/queues.py
@@ -0,0 +1,12 @@
+#Implementation of Queues
+
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        return self.items.insert(0, item)
+
